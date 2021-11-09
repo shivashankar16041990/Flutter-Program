7d722d9ed590efee4f86b1d5df256d9e8891b42a
Shivashankar Bhutekar <59565135+shivashankar16041990@users.noreply.github.com>
2021-11-10 00:27:29 +0530

    Add files via upload

---
+++ integration_test_flutter/lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  @override
+  int count = 0;
+
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+          body: Row(
+            children: [
+              ElevatedButton(key: Key("increment"),
+                  onPressed: () {
+                    setState(() {
+                      count++;
+                    });
+                  },
+                  child: Text("+")),
+              Text("$count",key: Key("counter")),
+              ElevatedButton(key: Key("decrement"),
+                  onPressed: () {
+                    setState(() {
+                      count--;
+                    });
+                  },
+                  child: Text("-"))
+            ],
+          ),
+        ));
+  }
+}
