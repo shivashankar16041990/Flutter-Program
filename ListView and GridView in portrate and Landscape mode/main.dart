c266fc459a4f891f1aae5e2c7cd4a6736705e526
Shivashankar Bhutekar <59565135+shivashankar16041990@users.noreply.github.com>
2023-01-21 14:54:29 +0530

    Update main.dart

--- ListView and GridView in portrate and Landscape mode/main.dart
+++ ListView and GridView in portrate and Landscape mode/main.dart
@@ -1,93 +1,47 @@
 import 'dart:io';
 
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart' ;
+import 'package:flutter/material.dart';
 
 void main() {
   runApp(myapp());
 }
 
-
 class myapp extends StatelessWidget {
-
-
-
-
   @override
   Widget build(BuildContext context) {
-    return
+    return MaterialApp(
+        title: "My App",
+        theme: ThemeData.light(),
+        home: Scaffold(body: MyList()));
+  }
+}
 
-      MaterialApp(
-
-          title:"My App",theme: ThemeData.light(),
-          home:Scaffold(
-              body:
-                        MyList()
-
-
-
-)      );
-
+class MyList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    if (MediaQuery.of(context).orientation == Orientation.landscape) {
+      debugPrint("    landscape");
+      return GridView.count(
+        crossAxisCount: 5,
+        children: generate_listview(),
+      );
+    } else {
+      debugPrint(" PORTRATE");
+      return ListView(children: generate_listview());
+    }
   }
 
-
+  List<Widget> generate_listview() {
+    List<Widget> list = List.empty(growable: true);
+    for (int i = 0; i < 20; i++)
+      list.add(Padding(
+          padding: EdgeInsets.all(10),
+          child: Center(
+              child: ListTile(
+                  title: Center(child: Text('item $i')),
+                  tileColor: Colors.black26,
+                  trailing: Icon(Icons.add_call)))));
+    return list;
+  }
 }
-
-
-class MyList extends StatelessWidget
-{
-  @override
-  Widget build(BuildContext context) {
-
-    if(MediaQuery.of(context).orientation==Orientation.landscape)
-      {
-
-        debugPrint("    landscape");
-        return GridView.count(
-            crossAxisCount: 5,
-            children:generate_listview()
-
-
-
-
-
-    ,);
-      }
-
-else {
-
-      debugPrint(" PORTRATE");
-  return
-      ListView(children:generate_listview());
-
-
-
-
-}
-}
-
-
-
-List<Widget> generate_listview()
-
-{
-  List<Widget> list =List.empty(growable: true) ;
-  for (int i=0;i<20;i++)
-    list.add(
-        Padding(
-            padding:EdgeInsets.all(10),
-            child:Center(
-                child:ListTile(
-                    title:Center(
-                        child:Text(
-                            'item $i')),
-                    tileColor:Colors.black26,
-                    trailing: Icon(
-                        Icons.add_call
-                    )))));
-  return list;
-
-
-}
-}
-
